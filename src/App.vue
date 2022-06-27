7379793e668f4e8a300e3cf10f94fcfb6d9c2bca
4lexRossi <devalexrossi@gmail.com>
2022-06-27 17:47:23 -0300

    started the todo page customization

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,71 @@
-<template>
-  <div>Hello</div>
-</template>
-
 <script setup>
+import { ref, onMounted, computed, watch } from 'vue';
 
+const todos = ref([]);
+const name = ref('');
+
+const input_content = ref('');
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+// eslint-disable-next-line vue/no-side-effects-in-computed-properties
+  todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt;
+  })
+);
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || '';
+});
 </script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="do some push ups"
+          v-model="input_content"
+        />
+        <h4>Pick a category</h4>
+
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
